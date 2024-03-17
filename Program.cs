9c7f4c4842539adcf3bcaa310e5491b268a3543b
AndreyCorpse28090 <280990corpse@gmail.com>
2024-03-17 15:36:03 +0300

    в

--- Program.cs
+++ Program.cs
@@ -10,12 +10,47 @@ namespace Array_Bukov
     {
         static void Main(string[] args)
         {
+            // Создание объекта класса Array с длиной 5 и вывод его размера
+            Array array1 = new Array(5);
+            Console.WriteLine("Размер массива: " + array1.Size);
+
+            // Вывод массива array1
+            Console.WriteLine("Array1:");
+            array1.Print();
+
+            // Установка значения 10 для всех элементов массива и вывод массива
+            array1.SetValue(10);
+            Console.WriteLine("Array1 после установки значения:");
+            array1.Print();
+
+            // Создание объекта класса ArrayList с емкостью 5 и вывод емкости
+            ArrayList arrayList1 = new ArrayList(5);
+            Console.WriteLine("ArrayList ёмкость: " + arrayList1.Capacity);
+
+            // до добавления элементов
+            Console.WriteLine("ArrayList1 до добавления элементов:");
+            arrayList1.Print();
+
+            // Добавление элементов в массив arrayList1 и вывод его содержимого
+            arrayList1.AddItem(5);
+            arrayList1.AddItem(6);
+            arrayList1.AddItem(9);
+            Console.WriteLine("ArrayList1 после добавления элементов:");
+            arrayList1.Print();
+
+            Console.WriteLine("ArrayList1 емкость до сжатия: " + arrayList1.Capacity);
+
+            // Сжатие массива arrayList1 и вывод его содержимого и емкости
+            arrayList1.Shrink();
+            Console.WriteLine("ArrayList1 после сжатия:");
+            arrayList1.Print();
+            Console.WriteLine("ArrayList1 емкость после сжатия: " + arrayList1.Capacity);
         }
     }
     public class Array
     {
         private const int DefaultLength = 10;
-        private int[] data;
+        protected int[] data;
 
         // Конструктор по умолчанию
         public Array()
@@ -39,7 +74,7 @@ namespace Array_Bukov
             }
         }
 
-        // Свойство для получения размера массива
+        // Свойство для получения размера массиваВ
         public int Size
         {
             get { return data.Length; }
@@ -55,7 +90,6 @@ namespace Array_Bukov
             Console.WriteLine();
         }
 
-        // получение значения элемента массива по индексу
         // Получение значения элемента массива по индексу
         public int GetItem(int index)
         {
@@ -96,28 +130,28 @@ namespace Array_Bukov
         // Конструктор по умолчанию, создает массив дефолтной емкости нулевой длины
         public ArrayList() : base()
         {
-            k = 1.5; // Пример значения для коэффициента расширения
+            k = 1.3; // Пример значения для коэффициента расширения
             capacity = 0; // Начальная емкость равна нулю
         }
 
         // Конструктор, создающий массив с изначальной емкостью
         public ArrayList(int capacity) : base(capacity)
         {
-            k = 1.5; // Пример значения для коэффициента расширения
+            k = 1.3; // Пример значения для коэффициента расширения
             this.capacity = capacity;
         }
 
         //конструктор, создающий массив заданной длины и емкости, заполненный значениями по умолчанию, если capacity < size то делать capacity = size;
         public ArrayList(int size, int capacity) : base(size > capacity ? size : capacity)
         {
-            k = 1.5; 
+            k = 1.3; 
             this.capacity = size > capacity ? size : capacity;
         }
 
         // конструктор, создающий массив заданной длины и емкости, заполненные переданными значением, если capacity < size то делать capacity = size
         public ArrayList(int size, int capacity, int value) : base(size > capacity ? size : capacity)
         {
-            k = 1.5; // Пример значения для коэффициента расширения
+            k = 1.3; // Пример значения для коэффициента расширения
             this.capacity = size > capacity ? size : capacity;
 
             // Заполнение массива переданным значением
@@ -126,6 +160,43 @@ namespace Array_Bukov
                 SetItem(i, value);
             }
         }
+
+        public void AddItem(int value)
+        {
+            if (Size >= capacity)
+            {
+                int newCapacity = (int)(capacity * k); // Вычисление новой ёмкости
+                Resize(newCapacity); // Изменение размера массива на новую емкость
+                capacity = newCapacity; // Обновление значения текущей ёмкости
+            }
+
+            Resize(Size + 1); // Увеличивю размер массива
+            SetItem(Size - 1, value); // устанавливаю значение в конец массива
+        }
+
+
+        // метод сжатия массива с уменьшением capacity до размера size
+        public void Shrink()
+        {
+            int newSize = Size;
+            Resize(newSize);
+            capacity = newSize;
+        }
+
+        private void Resize(int newSize)
+        {
+            int[] newData = new int[newSize]; // новый массив с новым размером
+
+            // копирование элементов из старого массива в новый
+            for (int i = 0; i < Size && i < newSize; i++)
+            {
+                newData[i] = GetItem(i);
+            }
+
+            data = newData; // Обновляю ссылку на массив
+        }
     }
 }
+     
+
 
